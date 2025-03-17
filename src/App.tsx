42075c3eefeff74d7c79a27977c9366961200077
David Sandoval <ing.david1993@gmail.com>
2025-03-16 23:48:21 -0500

    feat: :lipstick: configure Tailwind with initial theme support and base structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import Button from "./ui/button";
-
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <Button>button</Button>
-    </>
+    <div className="container">
+      <h3 className="text-white font-bold text-3xl mb-4">
+        Mi{" "}
+        <span className="bg-clip-text text-transparent bg-gradient-to-r from-purple-500 to-pink-500">
+          Experiencia
+        </span>
+      </h3>
+      <p className="italic font-light">contenido ejemplo</p>
+    </div>
   );
 }
 
